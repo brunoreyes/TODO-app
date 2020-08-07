124016b487a8460d75ae90ec21198839d3405653
“brunoreyes” <bruno619reyes@gmail.com>
2020-08-07 00:32:43 -0500

    added form components within ideasPage

--- src/components/IdeasPage/IdeasPage.js
+++ src/components/IdeasPage/IdeasPage.js
@@ -16,15 +16,22 @@ import {
   Input,
   InputLabel,
   FormHelperText,
+  Select,
+  MenuItem,
+  TextField,
+  Button,
 } from '@material-ui/core';
 import DeleteIcon from '@material-ui/icons/Delete';
 import EditIcon from '@material-ui/icons/Edit';
 import StarIcon from '@material-ui/icons/Star';
+import Icon from '@material-ui/core/Icon';
+import EmojiObjectsIcon from '@material-ui/icons/EmojiObjects';
 
 // BASIC CONVERTED MATERIAL UI TABLE
 // classes.tableHead, table, anything is just talking about styles
 // that are manipulated here
 const styles = (theme) => ({
+  allContainer: {},
   root: {
     width: '80%',
     marginTop: theme.spacing.unit * 3,
@@ -41,94 +48,238 @@ const styles = (theme) => ({
   tableHeadCell: {
     'font-size': '15px',
     'font-family': 'Montserrat',
-    // 'font-weight': 'bold',
+    'font-weight': '500',
     color: 'white',
-    'padding-top': '13px',
-    'padding-bottom': '13px',
-    'padding-left': '20px',
+    'padding-top': '18px',
+    'padding-bottom': '18px',
+    'padding-left': '18px',
   },
   tableCell: {
     'font-family': 'Montserrat',
     'font-size': '13px',
     'text-transform': 'capitalize',
-    'padding-left': '20px',
+    'padding-left': '18px',
     'padding-top': '10px',
     'padding-bottom': '10px',
   },
   tableCellDescription: {
     'font-family': 'Montserrat',
     'font-size': '13px',
-    'padding-left': '20px',
+    'padding-left': '15px',
   },
-  materialIcon: {
+  starIconForm: {
+    'font-size': '40px',
+    'padding-top': '20px',
+    'padding-left': '0px',
+    'padding-right': '15px',
+    '&:hover': {
+      color: 'gold',
+    },
+  },
+  starIconTable: {
+    'font-size': '20px',
+    'padding-top': '20px',
+    'padding-left': '0px',
+    'padding-right': '15px',
+    '&:hover': {
+      color: 'gold',
+    },
+  },
+  editIcon: {
     'font-size': '20px',
     'padding-top': '10px',
     'padding-bottom': '10px',
     'padding-left': '0px',
-    'padding-right': '20px',
+    'padding-right': '15px',
+  },
+  deleteIcon: {
+    'font-size': '20px',
+    'padding-top': '10px',
+    'padding-bottom': '10px',
+    'padding-left': '0px',
+    'padding-right': '15px',
+  },
+  formControl: { margin: '10px', 'text-align': 'center' },
+  formControlContainer: {
+    'text-align': 'center',
+    display: 'inline-block',
+    width: 'auto',
+    'margin-bottom': '100px',
+  },
+  menuItemsContainer: {
+    flexWrap: 'wrap',
+  },
+  menuItem: {
+    margin: theme.spacing.unit / 4,
+  },
+  invisibleText: {
+    color: 'white',
+  },
+  newOrAddIdeaButton: {
+    color: 'white',
+    'font-family': 'Montserrat',
+    'background-color': '#161616',
+    margin: '10px',
+    '&:hover': {
+      color: 'gold',
+      'background-color': '#161616',
+    },
+  },
+  uploadImageButton: {
+    'padding-top': '0px',
+    'padding-bottom': '0px',
+    'background-color': '#161616',
+    '&:hover': {
+      'background-color': '#161616',
+    },
   },
 });
+
+// MENU props handle the scrollable selector
+const ITEM_HEIGHT = 48;
+const ITEM_PADDING_TOP = 8;
+const MenuProps = {
+  PaperProps: {
+    style: {
+      maxHeight: ITEM_HEIGHT * 3.4 + ITEM_PADDING_TOP,
+      width: 150,
+    },
+  },
+};
 
 class IdeasPage extends Component {
   componentDidMount() {
     // use component did mount to dispatch an action to request the SearchList from the API
     this.props.dispatch({ type: 'FETCH_IDEAS' });
   }
+  constructor() {
+    super();
 
+    var today = new Date(),
+      date =
+        today.getFullYear() +
+        '-' +
+        (today.getMonth() + 1) +
+        '-' +
+        today.getDate();
+
+    this.state = {
+      date: date,
+    };
+  }
   render() {
     const { classes } = this.props;
     return (
-      <div>
+      <div className={classes.allContainer}>
         {/* THE FORM */}
-        <FormControl className={classes.formControl}>
-          <InputLabel htmlFor="component-helper">Name</InputLabel>
-          <Input
-            id="component-helper"
-            // value={this.state.name}
-            // onChange={this.handleChange}
-            aria-describedby="component-helper-text"
+        <div className={classes.formControlContainer}>
+          <Button
+            variant="contained"
+            // color="white"
+            className={classes.newOrAddIdeaButton}
+            endIcon={<EmojiObjectsIcon>New</EmojiObjectsIcon>}
+          >
+            New
+          </Button>
+          <FormControl className={classes.formControl}>
+            <InputLabel htmlFor="component-helper">Name *</InputLabel>
+            <Input
+              id="component-helper"
+              // value={this.state.name}
+              // onChange={this.handleChange}
+              aria-describedby="component-helper-text"
+            />
+            <FormHelperText id="component-helper-text">Required</FormHelperText>
+          </FormControl>
+          <FormControl className={classes.formControl}>
+            <InputLabel htmlFor="component-helper">Description *</InputLabel>
+            <Input
+              id="component-helper"
+              //   value={this.state.name}
+              //   onChange={this.handleChange}
+              aria-describedby="component-helper-text"
+            />
+            <FormHelperText id="component-helper-text">Required</FormHelperText>
+          </FormControl>
+          <FormControl className={classes.formControl}>
+            <InputLabel htmlFor="component-helper">Link</InputLabel>
+            <Input
+              id="component-helper"
+              // value={this.state.name}
+              // onChange={this.handleChange}
+              aria-describedby="component-helper-text"
+            />
+            <FormHelperText id="component-helper-text"></FormHelperText>
+          </FormControl>
+          <FormControl className={classes.formControl}>
+            <InputLabel htmlFor="component-helper">Image Url</InputLabel>
+            <Input
+              id="component-helper"
+              // value={this.state.name}
+              // onChange={this.handleChange}
+              aria-describedby="component-helper-text"
+            />
+            <FormHelperText id="component-helper-text"></FormHelperText>
+          </FormControl>
+          <Button
+            variant="contained"
+            // color="white"
+            className={classes.uploadImageButton}
+          >
+            <ImageUpload />
+          </Button>
+          <FormControl className={classes.formControl}>
+            <InputLabel>Category *</InputLabel>
+            <Select
+              MenuProps={MenuProps}
+              // value={this.state.age}
+              // onChange={this.handleChange}
+              //   input={<Input name="age" id="age-helper" />}
+            >
+              {/* <MenuItem value="">
+                <em>None</em>
+              </MenuItem> */}
+              <MenuItem value={1}>Relationship</MenuItem>
+              <MenuItem value={2}>Work</MenuItem>
+              <MenuItem value={3}>Personal Work</MenuItem>
+              <MenuItem value={4}>Fitness</MenuItem>
+              <MenuItem value={5}>Music</MenuItem>
+              <MenuItem value={6}>Art</MenuItem>
+              <MenuItem value={7}>School</MenuItem>
+              <MenuItem value={8}>Finance</MenuItem>
+              <MenuItem value={9}>Health</MenuItem>
+              <MenuItem value={10}>Art</MenuItem>
+              <MenuItem value={11}>New Category</MenuItem>
+            </Select>
+            <FormHelperText>
+              Required
+              <span className={classes.invisibleText}>
+                invisible text is here
+              </span>
+            </FormHelperText>
+          </FormControl>
+          <StarIcon className={classes.starIconForm}></StarIcon>
+          <TextField
+            id="date"
+            label="Date"
+            type="date"
+            // defaultValue={this.state.date} sets the calendar to today's date via
+            // the constructor function above
+            defaultValue={this.state.date}
+            className={classes.textField}
+            InputLabelProps={{
+              shrink: true,
+            }}
           />
-          <FormHelperText id="component-helper-text">
-            Some important helper text
-          </FormHelperText>
-        </FormControl>
-        <FormControl className={classes.formControl}>
-          <InputLabel htmlFor="component-helper">Name</InputLabel>
-          <Input
-            id="component-helper"
-            // value={this.state.name}
-            // onChange={this.handleChange}
-            aria-describedby="component-helper-text"
-          />
-          <FormHelperText id="component-helper-text">
-            Some important helper text
-          </FormHelperText>
-        </FormControl>
-        <FormControl className={classes.formControl}>
-          <InputLabel htmlFor="component-helper">Name</InputLabel>
-          <Input
-            id="component-helper"
-            // value={this.state.name}
-            // onChange={this.handleChange}
-            aria-describedby="component-helper-text"
-          />
-          <FormHelperText id="component-helper-text">
-            Some important helper text
-          </FormHelperText>
-        </FormControl>
-        <FormControl className={classes.formControl}>
-          <InputLabel htmlFor="component-helper">Name</InputLabel>
-          <Input
-            id="component-helper"
-            // value={this.state.name}
-            // onChange={this.handleChange}
-            aria-describedby="component-helper-text"
-          />
-          <FormHelperText id="component-helper-text">
-            Some important helper text
-          </FormHelperText>
-        </FormControl>
-
+          <Button
+            variant="contained"
+            // color="white"
+            className={classes.newOrAddIdeaButton}
+            endIcon={<EmojiObjectsIcon>Add</EmojiObjectsIcon>}
+          >
+            Add
+          </Button>
+        </div>
         {/* THE TABLE */}
         <div className="tableContainer"></div>
         <Paper className={classes.root}>
@@ -191,9 +342,9 @@ class IdeasPage extends Component {
                         {ideas.date.split('T')[0]}
                       </TableCell>
                       {/* Star is for favorite */}
-                      <StarIcon className={classes.materialIcon}></StarIcon>
-                      <EditIcon className={classes.materialIcon}></EditIcon>
-                      <DeleteIcon className={classes.materialIcon}></DeleteIcon>
+                      <StarIcon className={classes.starIconTable}></StarIcon>
+                      <EditIcon className={classes.editIcon}></EditIcon>
+                      <DeleteIcon className={classes.deleteIcon}></DeleteIcon>
                       {/* <p>{JSON.stringify(index)}</p>
                       <p>{JSON.stringify(ideas.name)}</p> */}
                     </TableRow>
